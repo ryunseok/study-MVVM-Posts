0a0f561616509e98395e4cee808d521b5d875d6f
ryunseok.kim <ryunseok.kim@nexstreaming.com>
2018-08-08 17:02:36 +0900

    Enable Databinding

--- app/src/main/java/com/example/ryunseok/mvvmposts/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/ryunseok/mvvmposts/utils/BindingAdapters.kt
@@ -2,6 +2,7 @@ package com.example.ryunseok.mvvmposts.utils
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
+import android.databinding.BindingAdapter
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.ryunseok.mvvmposts.utils.extension.getParentActivity
